e614e83ca382394641ea7c6b3fa1f6cba7f09eeb
isaias005 <isaias005@hotmail.com>
2020-07-28 19:46:08 -0400

    Fixed product card bug

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,7 +6,7 @@ const ProductCard = ({ product, API }) => {
   return (
     <Col className="pb-3 p-sm-3" xs={7} sm={6} md={4} lg={3}>
       <Card>
-        <Card.Img variant="top" src={product.imagen.url ? API + product.imagen.url : "https://via.placeholder.com/150x100"}></Card.Img>
+        <Card.Img variant="top" src={product.imagen ? API + product.imagen.url : "https://via.placeholder.com/150x100"}></Card.Img>
         <Card.Body>
           <Card.Title>{product.titulo}</Card.Title>
           <Card.Text className="text-truncate">{product.descripcion}</Card.Text>
