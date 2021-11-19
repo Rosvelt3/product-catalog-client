ec7076f8e2d95401cdbcb9244446830ce4828a0d
Isaias <isaias005@hotmail.com>
2021-11-18 21:44:17 -0400

    Product list fix

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -24,7 +24,7 @@ const ProductList = ({ maxProducts, search = "", reverse }) => {
   }, []);
 
   const renderProducts = () => {
-    if (!products) return false;
+    if (!Symbol.iterator in Object(products)) return false;
 
     let filteredProducts = [...products];
 
